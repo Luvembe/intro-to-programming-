046a014b3b406a311e65edb4e41ac885fb558714
Luvembe <102525085+Luvembe@users.noreply.github.com>
2022-03-31 17:39:09 +0300

    Add files via upload

---
+++ c program to add 5 numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c,d,e,f;
+ printf("Enter first number\n");
+ scanf("%d",&a);
+ printf("Enter second number\n");
+ scanf("%d",&b);
+printf("Enter second number\n");
+ scanf("%d",&c);
+printf("Enter second number\n");
+ scanf("%d",&d);
+printf("Enter second number\n");
+ scanf("%d",&e);
+ f=a+b+c+d+e;
+ printf("Add=%d",f);
+ 	
+}
+
---
+++ program to calculate area of a circle.c
@@ -0,0 +1,13 @@
+//program to calculate area of a circle by user
+#include<stdio.h>
+
+int main()
+{
+	float area,r;
+	printf("Enter radius of circle\n");
+	scanf("%f",&r);
+	area=3.14*r*r;
+	printf("Area of rectangle=%f",area);
+
+ return 0;
+}
