6903331700d430bd1b8cc4ae80f3fee8823a5ee3
Luvembe <102525085+Luvembe@users.noreply.github.com>
2022-04-01 13:20:00 +0300

    Add files via upload

---
+++ program to add 5 numbers by the user.c
@@ -0,0 +1,14 @@
+//program to calculate add numbers
+#include<stdio.h>
+int main()
+{
+int A=5;
+int B=7;
+int C=8;
+int D=4;
+int E=5;
+int sum;
+sum=A+B+C+D+E;
+printf("the sum of five numbers is %d",sum);
+return 0;
+}
